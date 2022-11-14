cce81b608a4360689b64d5161a78da228fbd0d80
Leo Salzmann <leo.salzmann@proton.me>
2022-11-14 13:04:26 +0100

    first MON commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../model/database/helper");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET all
+router.get("/", async function(req, res, next) {
+  db("SELECT * FROM userInfo;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
 });
 
-module.exports = router;
+
+// GET one user by ID (DB: userdata; table: userInfo)
+router.get("/:id", async function(req, res, next) { 
+  
+    let userId = req.params.id;
+  
+    try {
+      let results = await db(`SELECT * FROM userInfo WHERE id = ${userId}`);
+      let users = results.data;
+      if (users.length === 0) {
+        res.status(404).send({ error: "User not found" });
+      } else {
+        res.send(users[0]);
+      }
+    } catch (err) {
+      res.status(500).send({ error: err.message });
+    }
+  });
+
+  module.exports = router;
