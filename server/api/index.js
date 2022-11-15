871e79957698158abb5468bd1fcef8a14a890042
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-15 15:00:43 +0100

    fix(server): Fix api/index.js for deploy

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,7 @@ const usersRouter = require('../routes/users');
 const indexRouter = require("../routes");
 
 app.use(cors());
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,33 +1,32 @@
-var express = require("express");
-var router = express.Router();
-const db = require("../model/database/helper");
+const express = require('express');
+
+const router = express.Router();
+const db = require('../model/database/helper');
 
 // GET all
-router.get("/", async function(req, res, next) {
-  db("SELECT * FROM userInfo;")
-    .then(results => {
+router.get('/', async (req, res) => {
+  db('SELECT * FROM userInfo;')
+    .then((results) => {
       res.send(results.data);
     })
-    .catch(err => res.status(500).send(err));
+    .catch((err) => res.status(500).send(err));
 });
 
-
 // GET one user by ID (DB: userdata; table: userInfo)
-router.get("/:id", async function(req, res, next) { 
-  
-    let userId = req.params.id;
-  
-    try {
-      let results = await db(`SELECT * FROM userInfo WHERE id = ${userId}`);
-      let users = results.data;
-      if (users.length === 0) {
-        res.status(404).send({ error: "User not found" });
-      } else {
-        res.send(users[0]);
-      }
-    } catch (err) {
-      res.status(500).send({ error: err.message });
-    }
-  });
+router.get('/:id', async (req, res) => {
+  const userId = req.params.id;
 
-  module.exports = router;
+  try {
+    const results = await db(`SELECT * FROM userInfo WHERE id = ${userId}`);
+    const users = results.data;
+    if (users.length === 0) {
+      res.status(404).send({ error: 'User not found' });
+    } else {
+      res.send(users[0]);
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+module.exports = router;
