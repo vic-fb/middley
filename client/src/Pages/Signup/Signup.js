ac6bf92cea91eb0d79cb04f89233f297215c820c
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-16 22:44:16 +0100

    feat(signup): Update signup

--- client/src/Pages/Signup/Signup.js
+++ client/src/Pages/Signup/Signup.js
@@ -3,30 +3,34 @@ import {
   FormLabel,
   FormErrorMessage,
   FormHelperText,
-  Input, Container,
+  Input, Container, Button,
 } from '@chakra-ui/react';
+// import { useState } from 'react';
 
 function Signup() {
+  // [loading, setLoading] = useState(false);
+
   return (
     <Container>
       <form>
-        <FormControl>
+        <FormControl isRequired>
           <FormLabel>Name</FormLabel>
           <Input type="text" />
           <FormHelperText>What shall we call you?</FormHelperText>
           <FormErrorMessage>Name is required.</FormErrorMessage>
         </FormControl>
-        <FormControl>
+        <FormControl isRequired>
           <FormLabel>Email address</FormLabel>
           <Input type="email" />
           <FormHelperText>We&apos;ll never share your email.</FormHelperText>
           <FormErrorMessage>Email is required.</FormErrorMessage>
         </FormControl>
-        <FormControl>
+        <FormControl isRequired>
           <FormLabel>Password</FormLabel>
           <Input type="password" />
           <FormErrorMessage>Password is required.</FormErrorMessage>
         </FormControl>
+        <Button type="submit">Submit</Button>
       </form>
     </Container>
   );
