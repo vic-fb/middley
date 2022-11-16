ea651ca36d354480f743793a18fb8d577625fe55
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-16 22:45:43 +0100

    feat(activities): Remove css modules

--- client/src/Pages/Activities/Activities.js
+++ client/src/Pages/Activities/Activities.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Activities.module.css';
+import './Activities.css';
 
 function Activities() {
   const nav = useNavigate();
