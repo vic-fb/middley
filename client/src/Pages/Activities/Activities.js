41981a545c31551e26f9e724e195d20f7a32f518
Danielle Drake <danielledrake@mail.com>
2022-11-16 16:24:27 +0100

    react icons added in local view

--- client/src/Pages/Activities/Activities.js
+++ client/src/Pages/Activities/Activities.js
@@ -9,9 +9,9 @@ function Activities() {
     <div className="Activities">
       <h6>ACTIVITIES</h6>
       <form className="Activities-form">
-        <h3>Activity 1</h3>
-        <h3>Activity 2</h3>
-        <h3>Activity 3</h3>
+        <h3>Food</h3>
+        <h3>Drinks</h3>
+        <h3>Nature</h3>
         <button className="Submit" type="button" onClick={nav('/Results')}>
           Find Match
         </button>
