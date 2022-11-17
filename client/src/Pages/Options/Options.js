3d0bda074327b0a1c9432cfb2ec2fa1e2f82ef49
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-17 14:40:18 +0100

    fix(options): Mini change just so the deploy stops failing

--- client/src/Pages/Options/Options.js
+++ client/src/Pages/Options/Options.js
@@ -2,13 +2,17 @@
 // import { useNavigate } from 'react-router-dom';
 import './Options.module.css';
 
-// function Options = () => {
+function Options() {
 // const nav = useNavigate();
 // const handleClick = (event) => {
 //     event.preventDefault();
+  return (
+    <div />
+  );
+}
 
 // should return  a grid with three activity options from the yelp api.
 // the user should be able to click on one of the options and be redirected to the navigation page
 // the user should be able to click on the back button and be redirected to the Activities page
 
-// export default Options;
+export default Options;
