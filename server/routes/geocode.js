78dde8ade4af0172ee89d29c24d486f955da4151
Leo Salzmann <leo.salzmann@proton.me>
2022-11-17 11:27:41 +0100

    feat: midpoint calculation function in geocode route

--- server/routes/geocode.js
+++ server/routes/geocode.js
@@ -40,8 +40,17 @@ async function geocode(address) {
 
   return myresponse;
 }
+// arr1 will contain latitude for location 1 and longitude for location 1
+// arr2 will contain latitude for location 2 and longitude for location 2
+async function midpoint(arr1, arr2) {
+  // create var midlat = lat1 + lat2 (from arrays) divided by 2
+  const midlat = arr1[0] + arr2[0] / 2;
+  // create var midlng = lng1 + lng2 (from arrays) divided by 2
+  const midlng = arr1[1] + arr2[1] / 2;
+  // create array containing midlat and midlng
+  const midarr = [midlat, midlng];
+  // return new array with both
+  return midarr;
+}
 
-// async function midpoint(lat, lng) {
-// }
-
-export default geocode;
+export default { geocode, midpoint };
