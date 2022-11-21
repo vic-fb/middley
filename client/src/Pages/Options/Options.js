4ec6bf5f2c626aef5f2b01f35201bab5be8cc2be
Freya McLeish <freyamcleish@gmail.com>
2022-11-21 15:53:17 +0000

    yelp ratings and logo added

--- client/src/Pages/Options/Options.js
+++ client/src/Pages/Options/Options.js
@@ -18,6 +18,30 @@ import {
   Container,
 } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
+import yelpLogo from '../../common/assets/yelp_logo.png';
+import star0 from '../../common/assets/YelpStars/small_0.png';
+import star1 from '../../common/assets/YelpStars/small_1.png';
+import star1Half from '../../common/assets/YelpStars/small_1_half.png';
+import star2 from '../../common/assets/YelpStars/small_2.png';
+import star2Half from '../../common/assets/YelpStars/small_2_half.png';
+import star3 from '../../common/assets/YelpStars/small_3.png';
+import star3Half from '../../common/assets/YelpStars/small_3_half.png';
+import star4 from '../../common/assets/YelpStars/small_4.png';
+import star4Half from '../../common/assets/YelpStars/small_4_half.png';
+import star5 from '../../common/assets/YelpStars/small_5.png';
+
+const yelpStars = {
+  0: star0,
+  1: star1,
+  1.5: star1Half,
+  2: star2,
+  2.5: star2Half,
+  3: star3,
+  3.5: star3Half,
+  4: star4,
+  4.5: star4Half,
+  5: star5,
+};
 
 function Options({ activities }) {
   const nav = useNavigate();
@@ -96,9 +120,11 @@ function Options({ activities }) {
                     <Text py="1" fontSize="sm">
                       {activity.rating} / 5
                     </Text>
+                    <Image src={yelpStars[activity.rating]} />
                     <Text py="1" fontSize="sm">
                       {activity.reviewCount} ratings
                     </Text>
+                    <Image maxW="40px" src={yelpLogo} />
                   </Box>
                 </Stack>
               </CardBody>
