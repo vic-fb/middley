c714a10a783a4a6db53eea865909e5ed09cec8d1
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-21 20:04:11 +0100

    fix(api): Fix api url

--- client/src/common/helpers/auth.js
+++ client/src/common/helpers/auth.js
@@ -1,6 +1,6 @@
 import { post } from './crud';
 
-export const userLogin = (password, email) => post('/api/auth/login', { password, email });
+export const userLogin = (password, email) => post('/auth/login', { password, email });
 
 export const register = (username, email, password) => post('/auth/register', { username, email, password });
 
