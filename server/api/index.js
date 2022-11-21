1235dca86af06bbf5c1df608bd66d22b02951c4e
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-21 17:23:01 +0100

    fix(deploy): Test cors issue

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
 const app = require('express')();
-const cors = require('cors');
+// const cors = require('cors');
 
 const usersRouter = require('../routes/users');
 const indexRouter = require('../routes');
 
-app.use(cors());
+// app.use(cors());
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
