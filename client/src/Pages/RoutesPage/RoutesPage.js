c47a08592907389c9754d8109b1c61146d5e42ab
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-23 14:47:40 +0100

    feat(routes): Change map

--- client/src/Pages/RoutesPage/RoutesPage.js
+++ client/src/Pages/RoutesPage/RoutesPage.js
@@ -1,10 +1,6 @@
-// import the address1 and address2 values from the app.js file
-
 import { Link, useNavigate } from 'react-router-dom';
 import './RoutesPage.css';
 import {
-  Grid,
-  GridItem,
   AspectRatio,
   Breadcrumb,
   BreadcrumbItem,
@@ -18,18 +14,13 @@ import { ChevronRightIcon } from '@chakra-ui/icons';
 
 function RoutesPage({ activity }) {
   const nav = useNavigate();
-  // console.log(activity);
-
   function returnHomeClick() {
     nav('/');
   }
-
-  const routes = [
-    {
-      id: 1,
-      route: `https://www.google.com/maps/embed/v1/place?key=AIzaSyBY04gyee_CumsNrdlgV8_P9exvWMgSTc8&q=${activity.displayAddress}`,
-    },
-  ];
+  const searchString = encodeURI(`${activity.name} ${activity.displayAddress}`);
+  const routes = {
+    route1: `https://www.google.com/maps/embed/v1/place?key=AIzaSyBY04gyee_CumsNrdlgV8_P9exvWMgSTc8&q=${searchString}`,
+  };
 
   return (
     <Container>
@@ -66,28 +57,20 @@ function RoutesPage({ activity }) {
       </Breadcrumb>
 
       <div className="routes-page">
-        <h1>Routes</h1>
+        <Heading as="h1">Routes</Heading>
+        <Heading as="h2">'{activity.name}'</Heading>
+        <AspectRatio maxW="600px" ratio={4 / 3}>
+          <iframe
+            title="map"
+            src={routes.route1}
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+          />
+        </AspectRatio>
 
-        <Grid
-          templateColumns="repeat(2, 1fr)"
-          templateRows="repeat(3, 1fr)"
-          gap={4}
-        >
-          <GridItem colSpan={1} rowSpan={1}>
-            <Heading>'{activity.name}'</Heading>
-            <AspectRatio maxW="560px" ratio={16 / 9}>
-              <iframe
-                title="map"
-                src={routes[0].route}
-                width="560"
-                height="315"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-              />
-            </AspectRatio>
-          </GridItem>
-        </Grid>
         <Button onClick={returnHomeClick}>Return Home</Button>
       </div>
     </Container>
