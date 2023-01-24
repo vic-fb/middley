3ae842acc67a33e3e65aa181c79bcfb458de3ec8
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-01-24 20:22:47 +0100

    feat(home): Change layout

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,33 +1,27 @@
 import { Link } from 'react-router-dom';
 import {
-  Button, Heading, HStack, Image, VStack,
+  Box, Button, Heading, Image, VStack,
 } from '@chakra-ui/react';
 import appLogo from '../../common/assets/app-logo.png';
 import styles from './Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <VStack>
+    <Box>
+      <VStack spacing={12}>
         <Image
           className={styles.logo}
           src={appLogo}
           alt="A map with two pins, each with an arrow pointing to a heart in the middle of the map."
         />
-        <br />
-        <br />
-        <Heading p={4} fontSize="5xl">
+        <Heading as="h1" fontSize="5xl">
           Middley
         </Heading>
-        <br />
-        <br />
-        <HStack>
-          <Button size="lg" as={Link} to="/local" p={8} fontSize="1xl">
-            Get started!
-          </Button>
-        </HStack>
+        <Button as={Link} to="/local" fontSize="xl" size="lg" variant="outline" border="4px" borderColor="black">
+          Get started!
+        </Button>
       </VStack>
-    </div>
+    </Box>
   );
 }
 
