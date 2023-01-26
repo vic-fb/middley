0f44cfb289586be8ecd5590ce793eb7dd722250c
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-01-26 20:22:46 +0100

    feat(service-worker): Check SW update

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -72,3 +72,13 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener('fetch', (event) => {
+  // console.log('new fetch');
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
+});
