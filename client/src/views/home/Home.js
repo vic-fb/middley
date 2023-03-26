443e7e627620490bba6cc08889fea7f4f73e8d82
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-03-26 20:53:16 +0200

    style: update home view

--- client/src/views/home/Home.js
+++ client/src/views/home/Home.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import {
-  Box, Button, Heading, Image, VStack, Container
+  Box, Button, Heading, Image, VStack, Container,
 } from '@chakra-ui/react';
 import appLogo from '../../common/assets/app-logo.png';
 import styles from './Home.module.css';
@@ -8,24 +8,24 @@ import styles from './Home.module.css';
 function Home() {
   return (
     <Container>
-    <VStack spacing={12}>
-      <Box textAlign="center">
-        <Heading as="h1" fontSize="5xl">
-          middley
-        </Heading>
-        <Heading as="h2" fontSize="xl">
-          just meet in the middle
-        </Heading>
-      </Box>
-      <Image
-        className={styles.logo}
-        src={appLogo}
-        alt="A map with two pins, each with an arrow pointing to a heart in the middle of the map."
-      />
-      <Button as={Link} to="/main" fontSize="xl" size="lg" variant="outline" border="4px" borderColor="black">
-        Get started!
-      </Button>
-    </VStack>
+      <VStack spacing={12}>
+        <Box textAlign="center">
+          <Heading as="h1" fontSize="5xl">
+            middley
+          </Heading>
+          <Heading as="h2" fontSize="xl">
+            just meet in the middle
+          </Heading>
+        </Box>
+        <Image
+          className={styles.logo}
+          src={appLogo}
+          alt="A map with two pins, each with an arrow pointing to a heart in the middle of the map."
+        />
+        <Button as={Link} to="/main" fontSize="xl" size="lg" variant="outline" border="4px" borderColor="black">
+          Get started!
+        </Button>
+      </VStack>
     </Container>
   );
 }
