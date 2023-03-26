f1e5e154ee2d0a4d349b4315a3e414997332e7dd
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-03-27 01:15:26 +0200

    refactor: add stars

---
+++ client/src/views/stepper/components/options/components/stars/Stars.js
@@ -0,0 +1,23 @@
+import star0 from '../../../../../../common/assets/yelp-stars/small_0.png';
+import star1 from '../../../../../../common/assets/yelp-stars/small_1.png';
+import star1Half from '../../../../../../common/assets/yelp-stars/small_1_half.png';
+import star2 from '../../../../../../common/assets/yelp-stars/small_2.png';
+import star2Half from '../../../../../../common/assets/yelp-stars/small_2_half.png';
+import star3 from '../../../../../../common/assets/yelp-stars/small_3.png';
+import star3Half from '../../../../../../common/assets/yelp-stars/small_3_half.png';
+import star4 from '../../../../../../common/assets/yelp-stars/small_4.png';
+import star4Half from '../../../../../../common/assets/yelp-stars/small_4_half.png';
+import star5 from '../../../../../../common/assets/yelp-stars/small_5.png';
+
+export const ratingStars = {
+  0: star0,
+  1: star1,
+  1.5: star1Half,
+  2: star2,
+  2.5: star2Half,
+  3: star3,
+  3.5: star3Half,
+  4: star4,
+  4.5: star4Half,
+  5: star5,
+};
